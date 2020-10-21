a9d239a799903e2edfd715626d1fde005a4065db
Kirill <m-group-react@tutanota.com>
2020-10-22 01:31:45 +0300

    Updates

---
+++ precache-manifest.0f0b007e2d301827d8918e58eac8b5e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b40190a41df94b86a368055c1e476d",
+    "url": "/form-review/index.html"
+  },
+  {
+    "revision": "537ab00c9b4b049ad49a",
+    "url": "/form-review/static/css/main.72b498b9.chunk.css"
+  },
+  {
+    "revision": "a1f3f35daad3943769f1",
+    "url": "/form-review/static/js/2.219ee516.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/form-review/static/js/2.219ee516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537ab00c9b4b049ad49a",
+    "url": "/form-review/static/js/main.ba3e5182.chunk.js"
+  },
+  {
+    "revision": "c358c2fe6cbeec310659",
+    "url": "/form-review/static/js/runtime-main.cbbd102e.js"
+  }
+]);
